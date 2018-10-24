e0f5993297ac538f366474cf733d56dc32c9011c
Oskar Gewalli <oskar@gewalli.se>
2018-10-24 12:24:11 +0200

    Upgrade build.fsx

--- build.fsx
+++ build.fsx
@@ -2,98 +2,83 @@
 // FAKE build script
 // --------------------------------------------------------------------------------------
 
-#r @"packages/FAKE/tools/FakeLib.dll"
-open Fake
-open Fake.Git
-open Fake.AssemblyInfoFile
-open Fake.ReleaseNotesHelper
-open Fake.UserInputHelper
-open System
+
+#r "paket: groupref FakeBuild //"
+
+#load "./.fake/build.fsx/intellisense.fsx"
+
 open System.IO
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.DotNet.Testing
+open Fake.Tools
+open Fake.Api
+open Fake.DotNet
 
 // File system information 
 let solutionFile  = "elastic-studies.sln"
 
 // Pattern specifying assemblies to be tested using NUnit
-let testAssemblies = "**/bin/Release/*Tests.dll"
+let testProjects = ["./tests/Tests"]
+
+let configuration = "Release"
 
 // --------------------------------------------------------------------------------------
 // END TODO: The rest of the file includes standard build steps
 // --------------------------------------------------------------------------------------
 
-// Helper active pattern for project types
-let (|Fsproj|Csproj|Vbproj|) (projFileName:string) = 
-    match projFileName with
-    | f when f.EndsWith("fsproj") -> Fsproj
-    | f when f.EndsWith("csproj") -> Csproj
-    | f when f.EndsWith("vbproj") -> Vbproj
-    | _                           -> failwith (sprintf "Project file %s not supported. Unknown project type." projFileName)
-
-
-
-// Copies binaries from default VS location to expected bin folder
-// But keeps a subdirectory structure for each project in the 
-// src folder to support multiple project outputs
-Target "CopyBinaries" (fun _ ->
-    !! "src/**/*.??proj"
-    |>  Seq.map (fun f -> ((System.IO.Path.GetDirectoryName f) @@ "bin/Release", "bin" @@ (System.IO.Path.GetFileNameWithoutExtension f)))
-    |>  Seq.iter (fun (fromDir, toDir) -> CopyDir toDir fromDir (fun _ -> true))
-)
-
-// --------------------------------------------------------------------------------------
-// Clean build results
-
-Target "clean" (fun _ ->
-    CleanDirs ["bin"; "temp"; 
-            "Tests/bin/Release";
-            "Tests/bin/Debug";
-            "Tests/bin/Debug"
-            ] 
-)
-
-Target "build" (fun _ ->
-    !! solutionFile
-    |> MSBuildReleaseExt "" [("Platform", "Any CPU")] "Rebuild"
-    |> ignore
-)
-
-Target "test" (fun _ ->
-    !! testAssemblies
-    |> NUnit (fun p ->
-        { p with
-            DisableShadowCopy = true
-            TimeOut = TimeSpan.FromMinutes 20.
-            OutputFile = "TestResults.xml" })
+Target.create "clean" (fun _ ->
+    Shell.cleanDirs ["bin"; "temp"]
 )
 
 
-Target "pack" (fun _ ->
-    Paket.Pack(fun p -> 
-        { p with
-            OutputPath = "bin"})
+Target.create "restore" (fun _ ->
+    solutionFile
+    |> DotNet.restore id
 )
 
-Target "push" (fun _ ->
-    Paket.Push(fun p -> 
-        { p with
-            WorkingDir = "bin" })
+Target.create "build" (fun _ ->
+    let buildMode = Environment.environVarOrDefault "buildMode" configuration
+    let setParams (defaults:MSBuildParams) =
+        { defaults with
+            Verbosity = Some(Quiet)
+            Targets = ["Build"]
+            Properties =
+                [
+                    "Optimize", "True"
+                    "DebugSymbols", "True"
+                    "Configuration", buildMode
+                ]
+         }
+    MSBuild.build setParams solutionFile
 )
+
+Target.create "test_only" (fun _ ->
+    testProjects
+    |> Seq.iter (fun proj -> DotNet.test (fun p ->
+        { p with ResultsDirectory = Some __SOURCE_DIRECTORY__ }) proj)
+)
+
+Target.create "test" ignore
 
 
 // --------------------------------------------------------------------------------------
 // Run all targets by default. Invoke 'build <Target>' to override
 
-Target "all" DoNothing
-"clean"
-  ==> "pack"
+Target.create "all" ignore
+
+"test_only" ==> "test"
+
+"build" ?=> "test_only"
 
 "clean"
+  ==> "restore"
   ==> "build"
-  ==> "CopyBinaries"
   ==> "test"
   ==> "all"
 
-"pack"
-  ==> "push"
-
-RunTargetOrDefault "test"
+Target.runOrDefault "test"
